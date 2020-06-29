2ed597ea56c84821bcfcbb301d7e8df9c69be714
Casper <cms.vandenberg@gmail.com>
2020-06-29 18:23:55 +0200

    Added updated pom.xml file and added code for Spring Boot Application

--- src/main/java/com/javadevelopersguide/springboot/example/SpringBootApplicationLuncher.java
+++ src/main/java/com/javadevelopersguide/springboot/example/SpringBootApplicationLuncher.java
@@ -1,5 +1,20 @@
 package com.javadevelopersguide.springboot.example;
+import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
+
+import java.net.URISyntaxException;
+import org.thethingsnetwork.data.common.Connection;
+import org.thethingsnetwork.data.common.messages.ActivationMessage;
+import org.thethingsnetwork.data.common.messages.DataMessage;
+import org.thethingsnetwork.data.common.messages.UplinkMessage;
+import org.thethingsnetwork.data.mqtt.Client;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 /**
  * This Luncher for the spring boot application.
  *
@@ -8,6 +23,74 @@ import org.springframework.boot.SpringApplication;
  */
 public class SpringBootApplicationLuncher {
     public static void main(String[] args) {
+        String region = "eu";
+        String appId = "co2_sensor_stenden";
+        String accessKey = "ttn-account-v2.J5ws5KGhK9jVP5p56HfG1VyLka8PecrVTtIsam6MpWA"; // Top-Secret
+        String apiUrl = "https://loradashboardapi.herokuapp.com/add_measurement";
+
+        try
+        {
+            Client client = new Client(region, appId, accessKey);
+
+            client.onMessage((String devId, DataMessage data) -> {
+
+                try
+                {
+                    UplinkMessage message = (UplinkMessage) data;
+                    System.out.println(message);
+                    int co2 = (int)message.getPayloadFields().get("co2");
+                    double temperature = (double)message.getPayloadFields().get("temperature");
+                    double humidity = (double)message.getPayloadFields().get("humidity");
+                    int tvoc = (int)message.getPayloadFields().get("tvoc"); // hydrocarbons
+                    String DateTime = java.time.LocalDateTime.now().toString();
+                    JSONObject Obj = new JSONObject();
+                    Obj.put("datetime", DateTime);
+                    Obj.put("air_pressure", 0); // Node doesn't support air pressure
+                    Obj.put("humidity", humidity);
+                    Obj.put("temperature", temperature);
+                    Obj.put("hydrocarbons", tvoc);
+                    Obj.put("carbon_dioxide", co2);
+                    int nodeId = Integer.parseInt(devId);
+                    Obj.put("nodeID", nodeId);
+                    String jsonMessage = Obj.toString();
+                    System.out.println("Message json: " + (jsonMessage));
+
+                    HttpURLConnection conn = (HttpURLConnection) new URL(apiUrl).openConnection();
+                    conn.setDoOutput(true);
+                    conn.setRequestMethod("POST");// GET POST PUT DELETE
+                    conn.setRequestProperty("Content-Type", "application/json");
+                    try(OutputStream os = conn.getOutputStream()) {
+                        byte[] input = jsonMessage.getBytes(StandardCharsets.UTF_8);
+                        os.write(input, 0, input.length);
+                        System.out.println("Message sent to API");
+                    }catch (Exception out) {
+                        System.out.println("Something went wrong with sending the message!");
+                    }
+                    System.out.println(conn.getResponseCode()); // 200 - 299 (200)
+
+
+                }catch (Exception ex) {
+                    System.out.println("Cry in the bathroom: " + ex.getMessage());
+                    ex.printStackTrace();
+                }
+            });
+
+            client.onActivation((String _devId, ActivationMessage _data) -> System.out.println("Activation: " + _devId + ", data: " + _data.getDevAddr()));
+
+            client.onError((Throwable _error) -> System.err.println("error: " + _error.getMessage()));
+
+            client.onConnected((Connection _client) -> System.out.println("connected!"));
+
+            client.start();
+
+        } catch (URISyntaxException ex)
+        {
+            ex.printStackTrace();
+            System.out.println(ex.getMessage());
+        } catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
         SpringApplication.run(HelloWorldController.class, args);
     }
 }
